39b69c653a99ec051711da083ca7a7bd43bf7a16
saci zakaria <sacideveloppeur@gmail.com>
2020-12-27 12:08:14 +0100

    19. Les vues privées : L'espace membre de l'utilisateur :  throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/connection", name="app_login")
      * @param AuthenticationUtils $authenticationUtils
      * @return Response
      */
@@ -29,7 +29,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/deconnection", name="app_logout")
      */
     public function logout()
     {
