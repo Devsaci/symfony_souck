2bbf0f0772cedb01a10300b93b3f20fea0440578
saci zakaria <sacideveloppeur@gmail.com>
2020-12-27 21:11:07 +0100

    20. Les vues privées : Can't get a way to read the property "new_password" in class "App\Entity\User".

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -28,12 +29,32 @@ class ChangePasswordType extends AbstractType
                 'disabled' => true,
                 'label' => 'lastname'
             ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Mon mot de passe actuel',
-                'mapped' => false,
-                'attr' => [
-                    'placeholder' => 'Veuillez saisir votre mot de passe actuel'
+//            ->add('password', PasswordType::class, [
+//                'label' => 'Mon mot de passe actuel',
+//                'mapped' => false,
+//                'attr' => [
+//                    'placeholder' => 'Veuillez saisir votre mot de passe actuel'
+//                ]
+//
+//            ])
+
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mon nouveau Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre mot de passe.'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez Votre nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de confirmer votre nouveau mot de passe.'
+                    ]
                 ]
+
 
             ]);
     }
