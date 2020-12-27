39086aad783132d5f566d92c3c6de7eeab728fe6
saci zakaria <sacideveloppeur@gmail.com>
2020-12-27 22:17:24 +0100

    20. Les vues privées : notification

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -29,35 +29,35 @@ class AccountPasswordController extends AbstractController
      * @param UserPasswordEncoderInterface $encoder
      * @return Response
      */
-    
-    
-    public function index(Request $request,UserPasswordEncoderInterface $encoder): Response
-    {
 
+
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
+    {
+        $notification = null;
         $user = $this->getUser();
-        $form = $this->createForm(ChangePasswordType::class,$user);
+        $form = $this->createForm(ChangePasswordType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $old_pwd = $form->get('old_password')->getData();
             //dd($old_pwd);
-            if ($encoder->isPasswordValid($user, $old_pwd)){
+            if ($encoder->isPasswordValid($user, $old_pwd)) {
                 $new_pwd = $form->get('new_password')->getData();
                 //dd($new_pwd);
                 $password = $encoder->encodePassword($user, $new_pwd);
                 $user->setPassword($password);
-                $this->entityManager->persist($user);
+                //$this->entityManager->persist($user);
                 $this->entityManager->flush();
-
-
+                $notification = "Votre mot de passe a bien été mis à jour.";
+            } else {
+                $notification = "Votre mot de passe actuel n'est pas le bon";
             }
-
-
         }
 
 
-            return $this->render('account/password.html.twig', [
-            'form' => $form ->createView()
+        return $this->render('account/password.html.twig', [
+            'form' => $form->createView(),
+            'notification' => $notification
 
         ]);
     }
