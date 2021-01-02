008d1ba98575a6ffa873a582f8d3a39fcd59869e
saci zakaria <sacideveloppeur@gmail.com>
2021-01-02 21:05:46 +0100

    29. Mapping de l'entité Product() dans EasyAdmin: MoneyField::new('price')->setCurrency('EUR'),

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProductCrudController extends AbstractCrudController
@@ -18,10 +19,12 @@ class ProductCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
 
-return [
-    TextField::new('name'),
-    ImageField::new('illustration')
-
+        return [
+            TextField::new('name'),
+            ImageField::new('illustration'),
+            TextField::new('subtitle'),
+            TextareaField::new('description'),
+            MoneyField::new('price')->setCurrency('EUR'),
 
 ];
 
