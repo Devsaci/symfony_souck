947020b336f0416a81e08e2200ed7de1f8c48b59
saci zakaria <sacideveloppeur@gmail.com>
2021-01-02 21:00:47 +0100

    29. Mapping de l'entité Product() dans EasyAdmin :ImageField::new('illustration')

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+
+return [
+    TextField::new('name'),
+    ImageField::new('illustration')
+
+
+];
+
+    }
+
+}
